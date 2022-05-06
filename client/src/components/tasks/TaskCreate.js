0b7b586137fa9d0ebe9b9e9f34ddae18d475c7e2
athiraramanan <athiraramanan29@gmail.com>
2022-05-06 22:27:22 +0530

    Adding Form Validation

--- client/src/components/tasks/TaskCreate.js
+++ client/src/components/tasks/TaskCreate.js
@@ -2,44 +2,74 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class TaskCreate extends React.Component{
+	renderError({touched, error}) {
+		if (touched && error){
+			return(
+				<div className='ui error message'>
+					<div className='headder'>{error}</div>
+				</div>
+			)
+		}
+	}
 
-	renderTextInput({input, label}){
+	renderTextInput=({input, label, meta})=>{
+		const className=`field ${meta.error && meta.touched ? 'error' : ''}`
 		return( 
-			<div className='field'>
+			<div className={className}>
 				<label>{label}</label>
-				<input {...input}/>
+				<input {...input} autoComplete='off'/>
+				{this.renderError(meta)}
 			</div>
 		)
 	}
 	onSubmit(formValues){
-		console.log(formValues)
+		// console.log(formValues)
 	}
 
 	render(){
 		return(
-			<form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
+			<form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
 				{/*its a predifined component for using the different kinds of form*/}
-				<Field name='Task' component={this.renderTextInput} label='Enter Task'/>
+				<Field name='task' component={this.renderTextInput} label='Enter Task'/>
 				<div className='field'>
 					<label >Enter Project</label>
-					<Field name="Project" component="select" label='Enter Project'>
-	          <option />
+					<Field name="project" component="select" label='Enter Project'>
 	          <option value="Learning">Learning</option>
 	          <option value="Other Activities">Other Activities</option>
 	          <option value="Leave">Leave</option>
 	          <option value="Local Holiday">Local Holiday</option>
         	</Field>
         </div>
-				<Field name='Hours' component={this.renderTextInput} label='Enter Hours'/>
-				<Field name='Date' component={this.renderTextInput} label='Enter Date'/>
-				<Field name='Description' component={this.renderTextInput}  label='Enter Description'/>
+				<Field name='hours' component={this.renderTextInput} label='Enter Hours'/>
+				<Field name='date' component={this.renderTextInput} label='Enter Date'/>
+				<Field name='description' component={this.renderTextInput}  label='Enter Description'/>
 				<button className='ui button primary'>Submit</button>	
 			</form>
 		)
 	}	
 }
+
+const validate = (formValues) =>{
+	console.log(formValues)
+	const errors={};
+	if (!formValues.task){
+		errors.task='You must enter task'
+	}
+	if (!formValues.hours){
+		errors.hours='You must enter hours'
+	}
+	if (!formValues.date){
+		errors.date='You must enter date'
+	}
+	if (!formValues.description){
+		errors.description='You must enter description'
+	}
+	return errors;
+}
+
 export default  reduxForm({
 	// name of form is just a string that represnet the functionality of this component
 	// it may be in any format like task, Task, TASK etc
-	form: 'TaskCreate'
+	form: 'TaskCreate',
+	validate
 })(TaskCreate);
